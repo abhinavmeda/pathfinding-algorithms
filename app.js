272ea0063b2458b069fef50572c7652eb2bf0e6c
abhinavmeda <39966275+abhinavmeda@users.noreply.github.com>
2022-05-07 20:07:52 -0400

    added and modified a few files

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ let sliderVal = document.getElementById("grid-picker");
 sliderVal.value = 100;
 let labelVal = document.getElementsByClassName("label");
 let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext('2d');
+
+class cell {
+
+}
 sliderVal.oninput = function (){
     let newDimensions = sliderVal.value;
     for(let i = 0; i < labelVal.length; i++){
@@ -9,5 +14,11 @@ sliderVal.oninput = function (){
     }
     canvas.height = newDimensions;
     canvas.width =newDimensions;
+    
+    for(let i = 0; i < canvas.height; i++){
+        for(let j = 0; j < canvas.height; j++){
+            
+        } 
+    }
 
 }
