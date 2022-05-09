914ad4b8cc20420591b7dd64830ec326079bdd23
Abhinav Meda <abhinavmeda@Abhinavs-Air.phub.net.cable.rogers.com>
2022-05-09 18:25:39 -0400

    got the start, end, wall buttons to work and show up on the screen correctly

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 let grid = document.createElement("table");
 grid.id = 'grid';
-let c = 0;
+
+let keypress = null;
+document.body.addEventListener("keydown", (e) => {
+    if(e.key === "s"){
+        keypress = "s";
+    }
+    else if(e.key === "e"){
+        keypress = "e";
+    }
+    else if(e.key === "w"){
+        keypress = "w";
+    }
+})
+let start = false;
+let end = false;
+
 for(let i = 0; i < 20; i++){
     let tr = grid.appendChild(document.createElement("tr"));
     for(let j = 0; j < 20; j++){
         let cell = tr.appendChild(document.createElement("td"));
         cell.addEventListener('click', () => {
-            cell.style.backgroundColor = "black";
-        })
+            if(keypress === "s" && start === false){
+                cell.style.backgroundColor = "red";
+                start = true;
+                keypress = null;
+            }
+            else if(keypress === "e" && end === false){
+                cell.style.backgroundColor = "blue";
+                end = true;
+                keypress = null;
+            }
+        });
+        cell.addEventListener('mouseover', () => {
+            if(keypress === "w" && cell.style.backgroundColor === ""){
+                cell.style.backgroundColor = "black";
+            }
+        });
     }
 }
 document.body.appendChild(grid);
